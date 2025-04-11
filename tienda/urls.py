ab736d73ed73f12e1d874e8a1a9e688d502ee81f
JNROME2 <julian-nromero@hotmail.com>
2025-04-11 20:49:27 -0300

    add dos templates

--- tienda/urls.py
+++ tienda/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from tienda.views import sucursales
+from tienda.views import sucursales, productos, clientes
 
 urlpatterns = [
-    path("sucursales", sucursales, name= "sucursales")
+    path("sucursales", sucursales, name= "sucursales"),
+    path("productos", productos, name= "productos"),
+    path("clientes", clientes, name= "clientes"),
 ]
--- tienda/views.py
+++ tienda/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def sucursales (request):
     return render(request, "tienda/sucursales.html")
+
+def productos (request):
+    return render(request, "tienda/productos.html")
+
+def clientes (request):
+    return render(request, "tienda/clientes.html")
